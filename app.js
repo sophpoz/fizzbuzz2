d85f1f2864ae5e7c4cfef6958d11d54f430a451a
Sophia Pozzi <sopozzi@gmail.com>
2016-07-12 21:05:26 -0700

    updates

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
+function promptUser(){
+  var hey = prompt('Instructions: please enter an integer');
+  hey = parseInt(hey);
+  // VALIDATE!
+    return hey;
+  
+};
 
-$(document).ready(function(){
-	function intro(){
-	var hey = prompt('Instructions: please submit an integer below', '2');
-document.write('<ul>')
-	for(var i = 1; i <= hey; i++){
-	  if (i % 5 === 0 && i % 3 === 0){
-			document.write('<li> fizzbuzz </li>' )
-		} else if ( i % 3 === 0) {
-			document.write('<li> fizz </li>');
-		} else if( i % 5 === 0){
-			document.write('<li> buzz </li>');
-		} else {
-			document.write('<li>' + i + '</li>');
-		}
-	}
-	document.write('</ul>')
-}
-intro();
-});
-	// document.write('<ul>')
-	// for(var i = 1; i <= 100; i++){
-	//   if (i % 5 === 0 && i % 3 === 0){
-	// 		document.write('<li> fizzbuzz </li>' )
-	// 	} else if ( i % 3 === 0) {
-	// 		document.write('<li> fizz </li>');
-	// 	} else if( i % 5 === 0){
-	// 		document.write('<li> buzz </li>');
-	// 	} else {
-	// 		document.write('<li>' + i + '</li>');
-	// 	}
-	// }
-	// document.write('</ul>')
 
-	
+var usersNumber = promptUser();
+
+function fizzbuzz(hey){
+    document.write('<ul>')
+        for(var i = 1; i <= hey; i++){
+      if (i % 5 === 0 && i % 3 === 0){
+            document.write('<li> fizzbuzz </li>' )
+        } else if ( i % 3 === 0) {
+            document.write('<li> fizz </li>');
+        } else if( i % 5 === 0){
+            document.write('<li> buzz </li>');
+        } else {
+            document.write('<li>' + i + '</li>');
+        }
+    }
+    document.write('</ul>')
+};
+fizzbuzz(usersNumber); 
+
+
